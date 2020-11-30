48cf4a0479a2402fe522c4335bbe095de626314b
Daniel Grigo <daniel.grigo@freenet.de>
2020-11-30 16:55:27 +0100

    Build to deploy on gh pages

--- src/index.js
+++
@@ -1,8 +0,0 @@
-//main entry point into the app
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.js';
-import './css/normalize.css';
-import './css/styles.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
